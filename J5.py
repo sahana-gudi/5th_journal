6cc1517e0d7d04546f561576bee59a27da0447b7
sahana-gudi <sonugudi472@gmail.com>
2025-11-28 20:53:01 +0530

    Update J5.py

--- J5.py
+++ J5.py
@@ -1,18 +1,12 @@
 import sys
 
-# If arguments are provided, use them
-if len(sys.argv) == 4:
-    P = float(sys.argv[1])   # Principal
-    R = float(sys.argv[2])   # Rate of interest (%)
-    T = float(sys.argv[3])   # Time (years)
-else:
-    # Fall back to interactive input
-    P = float(input("Enter principal amount: "))
-    R = float(input("Enter rate of interest (%): "))
-    T = float(input("Enter time (years): "))
+if len(sys.argv) != 4:
+    print("Usage: python simple_interest.py <principal> <rate> <time>")
+    sys.exit(1)
 
-# Calculate Simple Interest
+P = float(sys.argv[1])
+R = float(sys.argv[2])
+T = float(sys.argv[3])
+
 SI = (P * R * T) / 100
-
-# Output
 print("Simple Interest =", SI)
