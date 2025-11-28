ac08d21b16d7d0e8983754c5870a6cbd974a8d2b
sahana-gudi <sonugudi472@gmail.com>
2025-11-28 20:21:51 +0530

    Update J5.py

--- J5.py
+++ J5.py
@@ -1,6 +1,10 @@
+import numpy as np
+values = np.array([
+    float(input("Enter the principal amount: ")),
+    float(input("Enter the rate of interest (in %): ")),
+    float(input("Enter the time (in years): "))
+])
 
-principal = float(input("Enter the principal amount: "))
-rate = float(input("Enter the rate of interest (in %): "))
-time = float(input("Enter the time (in years): "))
+principal, rate, time = values
 simple_interest = (principal * rate * time) / 100
 print(f"Simple Interest = {simple_interest}")
